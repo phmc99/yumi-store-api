cdf8a63519131a77a4c62d0206242df6cf13261b
Pedro Costa <phmc99@outlook.com>
2021-09-08 00:03:31 -0300

    feat: user authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const authConfig = require("../config/auth.json");
 const User = require("../models/users");
 
 const router = express.Router();
+
+const generateToken = (params = {}) => {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+};
 
 router.post("/register", async (req, res) => {
   const { email, cpf } = req.body;
@@ -18,10 +28,29 @@ router.post("/register", async (req, res) => {
     user.cpf = undefined;
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ user, token: generateToken({ id: user.id }) });
   } catch (err) {
     return res.status(400).send({ error: "Registration failed" });
   }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(400).send({ error: "User not found" });
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({ error: "Invalid password" });
+  }
+
+  user.cpf = undefined;
+  user.password = undefined;
+
+  res.send({ user, token: generateToken({ id: user.id }) });
 });
 
 module.exports = (app) => app.use("/auth", router);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 require("./controllers/authController")(app);
+require("./controllers/projectController")(app);
 
 console.log("API está funcionando!");
 app.listen(3001);
