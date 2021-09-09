fad92c4ec2e698c55134e6a4126937b072c80d57
Pedro Costa <phmc99@outlook.com>
2021-09-09 01:51:30 -0300

    fix: database uri

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost/yumistore");
+mongoose.connect(
+  "mongodb+srv://deploy:yumistoreapideploy@cluster0.ttan3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
