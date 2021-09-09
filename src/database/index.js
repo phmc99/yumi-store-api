20da8c20061c6f862acb341635397f74dc769155
Pedro Costa <phmc99@outlook.com>
2021-09-09 02:44:04 -0300

    deploy: first deploy heroku

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(
-  "mongodb+srv://deploy:yumistoreapideploy@cluster0.ttan3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
